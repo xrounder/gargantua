27f48c1938a1b0a9d2c97a0dec8557a1a0a62054
PhonierDeluxe <d.czogalla@gmx.de>
2014-11-20 15:01:22 +0100

    Treenode

---
+++ Hochschulprogramme/src/de/hs_mannheim/IB/TPE/WS14/Czogalla_Caballero/AssoziativesArray/AssociativeArray.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package de.hs_mannheim.IB.TPE.WS14.Czogalla_Caballero.AssoziativesArray;
+
+/**
+ * @author Dennis
+ *
+ */
+public interface AssociativeArray {
+
+	
+	
+}
---
+++ Hochschulprogramme/src/de/hs_mannheim/IB/TPE/WS14/Czogalla_Caballero/AssoziativesArray/Tree.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package de.hs_mannheim.IB.TPE.WS14.Czogalla_Caballero.AssoziativesArray;
+
+/**
+ * @author Dennis
+ *
+ */
+public class Tree {
+
+
+	class Node{
+		
+		private String key;
+		private int value;
+		
+		private Node left;
+		private Node right;
+	
+		
+		public Node(String key){
+			
+			this.key = key;
+			
+		}
+		
+		
+		
+		
+		
+		public Node setNode(){
+			
+			
+//			return new Node
+		}
+		
+		
+		public String getValue(){
+			
+			return key;
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+}
