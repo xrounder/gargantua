7f20f2717d2e1b61a99009c3dfa4020ac5b6e157
PhonierDeluxe <d.czogalla@gmx.de>
2014-11-21 01:17:43 +0100

    Tree

--- Hochschulprogramme/src/de/hs_mannheim/IB/TPE/WS14/Czogalla_Caballero/AssoziativesArray/Tree.java
+++ Hochschulprogramme/src/de/hs_mannheim/IB/TPE/WS14/Czogalla_Caballero/AssoziativesArray/Tree.java
@@ -19,24 +19,52 @@ public class Tree {
 		private Node right;
 	
 		
-		public Node(String key){
+		public Node(String key, Node node1st, Node node2nd){
 			
 			this.key = key;
+			setNode(node1st, node2nd);
+			
+		}
+		
+		public Node(String key, Node node){
+			
+			this(key,node, null);
+			
+		}
+		
+		public Node(String key){
+			
+			this(key, null, null);
 			
 		}
 		
 		
+		public void setNode(Node node){
 		
-		
-		
-		public Node setNode(){
+			left = node;
 			
-			
-//			return new Node
 		}
 		
+		public void setNode(Node node1st, Node node2nd){
+			
+			if((node1st != null && node2nd != null) && node1st.getValue() > node2nd.getValue()){
+				
+				setNode(node2nd,node1st);
+			}
+			
+			left = node1st;
+			right = node2nd;
+		}
+
 		
-		public String getValue(){
+		
+		
+		public int getValue(){
+			
+			return value;
+		}
+		
+		public String getKey(){
 			
 			return key;
 		}
