342b2f5d43984867ecbebb23696686c1816f868c
PhonierDeluxe <d.czogalla@gmx.de>
2014-11-21 20:22:03 +0100

    Überarbeitung von Binärbaum und Assoziatives Array

--- Hochschulprogramme/src/de/hs_mannheim/IB/TPE/WS14/Czogalla_Caballero/AssoziativesArray/AssociativeArray.java
+++ Hochschulprogramme/src/de/hs_mannheim/IB/TPE/WS14/Czogalla_Caballero/AssoziativesArray/AssociativeArray.java
@@ -7,8 +7,48 @@ package de.hs_mannheim.IB.TPE.WS14.Czogalla_Caballero.AssoziativesArray;
  * @author Dennis
  *
  */
-public interface AssociativeArray {
+public class AssociativeArray implements IAssociativeArray {
 
+	private Tree associativeArray;
+	
+	public AssociativeArray(int size){
+		
+		associativeArray = new Tree();
+	}
+	
+	
+	public void clear (){};
+	public boolean containsValue (){
+		
+		return false;
+	};
+	public boolean containsKey (){
+		
+		return false;
+	};
+	public int get (String key){
+		
+		return 0;
+	};
+	public boolean isEmpty (){
+		
+		return false;
+	};
+	public void put (String key, int value){};
+	public void putAll (){};
+	public int remove (String key){
+		
+		return 0;
+	};
+	public int size (){
+		
+		return 0;
+	};
+	public void update (String key, int value){};
+	public void forEach (){};
+	public void extractAll(){};
+	public void map (){};
+	
 	
 	
 }
--- Hochschulprogramme/src/de/hs_mannheim/IB/TPE/WS14/Czogalla_Caballero/AssoziativesArray/Tree.java
+++ Hochschulprogramme/src/de/hs_mannheim/IB/TPE/WS14/Czogalla_Caballero/AssoziativesArray/Tree.java
@@ -3,6 +3,10 @@
  */
 package de.hs_mannheim.IB.TPE.WS14.Czogalla_Caballero.AssoziativesArray;
 
+import java.lang.reflect.GenericDeclaration;
+
+import org.omg.CORBA.portable.ValueBase;
+
 /**
  * @author Dennis
  *
@@ -10,14 +14,15 @@ package de.hs_mannheim.IB.TPE.WS14.Czogalla_Caballero.AssoziativesArray;
 public class Tree {
 
 
-	class Node{
-		//
+	class Node<T>{
+		
 		private String key;
-		private int value;
+		private T value;
+		private int hash;
 		
 		private Node left;
 		private Node right;
-	
+
 		
 		public Node(String key, Node node1st, Node node2nd){
 			
@@ -47,7 +52,7 @@ public class Tree {
 		
 		public void setNode(Node node1st, Node node2nd){
 			
-			if((node1st != null && node2nd != null) && node1st.getValue() > node2nd.getValue()){
+			if((node1st != null && node2nd != null) && node1st.getHash() > node2nd.getHash()){
 				
 				setNode(node2nd,node1st);
 			}
@@ -58,8 +63,12 @@ public class Tree {
 
 		
 		
+		public int getHash(){
+			
+			return hash;
+		}
 		
-		public int getValue(){
+		public T getValue(){
 			
 			return value;
 		}
